c0a6029fbf808b1c9c240766287e0dccd883d0fb
noir-h <rose_s98@icloud.com>
2022-02-11 14:43:33 +0900

    add someone

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './App.module.css';
-import Auth from "./features/auth/Auth"
+import Core from "./features/core/Core"
 
 function App() {
   return (
     <div className='styles.app'>
-      <Auth></Auth>
+      <Core></Core>
     </div>
   );
 }
---
+++ project/src/features/core/Core.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Auth from '../auth/Auth'
+
+const Core: React.VFC = () => {
+  return (
+    <>
+      <Auth></Auth>
+    </>
+  )
+}
+
+export default Core
